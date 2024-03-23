6d0082c984cd2a401d228d46b2eba5538c372cdf
Vishal Verma <103056988+VishalVKV99@users.noreply.github.com>
2024-03-23 23:03:44 +0000

    type Conversion

---
+++ 01_Basics/03.js
@@ -0,0 +1,31 @@
+let number = 46
+console.log(typeof number)
+let number2 = "48"
+console.log(typeof number2)
+let number3 = Number(number) // type conversion of integer to number
+
+console.log(typeof number3)
+
+let macId = 23050
+ // let idValue = Number(mcaId)  // her etype conversion is not allowed 
+                                // if a number has also string attached to it
+console.log(typeof macId)
+
+// 34sf -> is  type of NaN(not a number)
+// true -> 1
+// false-> 0
+let logIn = 1
+let logOff = 0
+let check = Boolean(logIn)
+let check2 = Boolean(logOff)
+
+console.log(check)
+console.log(check2)
+ let idName = " "
+ console.log(idName) // it will give false if no string assigned to it
+ // " " -> return false if no value assigned to it
+ //  "vishal" -> true
+//  we can also conver string to number 
+
+
+// ********************  Operations  ****************************
