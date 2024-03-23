804b3d59734b0760a75e808a1eb6e096e2a0da13
Vishal Verma <103056988+VishalVKV99@users.noreply.github.com>
2024-03-23 22:33:51 +0000

    basics

---
+++ 01_Basics/02.js
@@ -0,0 +1,29 @@
+"use strict"; // will treat whole file as newer version of java script
+//  alert(3+3) // will not use in node it has different wAY to represent
+                // because be are using node js not browser
+
+// Make sure that out code will High Readability 
+
+let name = "Vishal" // it will show string type name
+let age = 21        // shows numerical type
+let male = true     // shows boolean type
+// All these above are types of let to use
+
+// number ->> range is 2 to the pow 53
+// bigint -> is used for very large number. eg.->stock market,reddit etc
+// string -> may be used in ' ' or " " where " "is preferred for best
+/// boolean -> true / false
+//null -> standalone value(representation of empty value).
+// undefined -> when value is not defined
+// symbol -> used to define unique 
+// typeof -> is used to shoe type of data
+console.log(typeof"Vishal")
+console.log(typeof age)
+console.log(typeof male)
+// INTERVIEW QSN
+console.log(typeof null)   //object
+console.log(typeof undefined)  // undefined
+
+
+
+
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,16 @@
 console.log("Vishal")
+const mobileNum = 987654321    // it wil not change at any cost and assign only 
+                            // integer value
+let mailId = "kissme@gmail.com"  //if we declare again any this variable 
+                                //again thenit will show error
+
+var empName = "Mukesh Ambani"  // it will create same variable name and
+                            // will new assigned to new variable 
+cityLived = "Delhi"           // declare variable without any datatype
+                            // its a bad practice
+
+console.table([mobileNum,mailId,empName,cityLived])
+/*
+if we declare any variable in JS without
+ assigning any value in it then it will show Undefined.
+*/
