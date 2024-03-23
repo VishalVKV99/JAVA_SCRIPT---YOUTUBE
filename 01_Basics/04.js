85796a8516f553d64e4f38360c19ae1ed0798647
Vishal Verma <103056988+VishalVKV99@users.noreply.github.com>
2024-03-23 23:58:24 +0000

    comparision

---
+++ 01_Basics/04.js
@@ -0,0 +1,28 @@
+console.log(2>1)
+console.log(1>2)
+console.log("2" > 1 )// it will automatically typeCOnvert String to number
+console.log(" 02" < 1)
+
+console.log("Comprasion with Null")
+
+
+console.log(null > 0)
+console.log(null < 0)  
+
+console.log(null ==  0)
+console.log(null >= 0)  // here valuse conversion takes place
+console.log(null <= 0)
+
+//   Above (> ,<, ==) -> null to number treates as 0
+// while (<=,>=) -> is true
+console.log(undefined == 0) //is false for all (<,>,==, <=,>=)
+
+//   ===(triple check) -> it checks strictly to our given values
+console.log( "2 " === 2) // false it strictly checks here
+                            // srting == number -> false
+                           
+
+
+
+
+
